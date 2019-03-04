b2c5ea76c364eb3dab597d590be66f48cdcb7bd1
Cosmo Kent <cosmolightfoot@gmail.com>
2019-03-04 15:22:45 -0800

    loadMovieList works with hardcoded data

--- src/make-ghibli-template.js
+++ src/make-ghibli-template.js
@@ -4,3 +4,12 @@ export default function renderIndexHtmlTemplate(movieName) {
     template.innerHTML = html;
     return template.content;
 }
+
+export function loadMovieList(jsonMovieArray) {
+    const filmListNode = document.getElementById('film-list');
+    jsonMovieArray.forEach(movieObject => {
+        console.log('hi', filmListNode);
+        const dom = renderIndexHtmlTemplate(movieObject);
+        filmListNode.appendChild(dom);
+    });
+}
--- test/file2.test.js
+++ test/file2.test.js
@@ -38,19 +38,19 @@ export default function renderDetailHtmlTemplate(movieName) {
 
 test('movie detail template', function(assert) {
 //arrange
-const movieName = { 
-    "title": "Castle in the Sky",
-    "id": "2baf70d1-42bb-4437-b551-e5fed5a87abe",
-    "description": "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.",
-    "director": "Hayao Miyazaki",
-    "producer": "Isao Takahata",
-    "release_date": "1986",
-    "rt_score": "95"
-};
+    const movieName = { 
+        "title": "Castle in the Sky",
+        "id": "2baf70d1-42bb-4437-b551-e5fed5a87abe",
+        "description": "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.",
+        "director": "Hayao Miyazaki",
+        "producer": "Isao Takahata",
+        "release_date": "1986",
+        "rt_score": "95"
+    };
 
 //act
-const result = renderDetailHtmlTemplate(movieName);
-const expected = /*html*/`
+    const result = renderDetailHtmlTemplate(movieName);
+    const expected = /*html*/`
     <section>
         <section class="title-container">
             <dl>Title:</dl>
