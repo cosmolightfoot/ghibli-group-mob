2f8c1a098cfbf48fe35c36a778db8be9488c8c04
Cosmo Kent <cosmolightfoot@gmail.com>
2019-03-04 12:03:10 -0800

    test setup

--- test/file1.test.js
+++ test/file1.test.js
@@ -1,7 +1,15 @@
 const test = QUnit.test;
 
-QUnit.module('test suite two');
+QUnit.module('construct html fill list');
 
-test('time to write a test', function(assert) {
-    assert.equal(true, false);
+test('make dynamic html list', function(assert) {
+    //arrange
+    const json = { "title": "My Neighbor Totoro" };
+    //act
+    const result = renderIndexHtmlTemplate(json);
+    const expected = `<li class="film-item"><a href="./assets/alchemy-logo.png">TBD</a></li>`;
+    //assert
+
+
+    assert.equal(result, expected);
 });
